22f83cb6aa3832acb3fee7458e296f8c23f4fc22
JL BERRIO <jlberrio2018@gmail.com>
2024-07-18 22:10:40 -0500

    3_Tercer: configuración pagina inicio

--- inventario/productos/views.py
+++ inventario/productos/views.py
@@ -4,3 +4,10 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def holaMundo(request):
     return HttpResponse("Hola Mundo ADSO")
+
+def inicio(request):
+    return render(request, 'inicio.html')
+
+def lista_productos(request):
+    productos_list = Producto.objects.all()
+    return render(request, 'productos.html', {'productos': productos_list})
